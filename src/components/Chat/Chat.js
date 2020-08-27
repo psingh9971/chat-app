99e536dcce9ee69739eb5a56d6d88e37a0b0a14d
psingh9971 <psingh997101@gmail.com>
2020-08-27 08:33:33 -0700

    g

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'https://parvinder-app.herokuapp.com/';
+  const ENDPOINT = 'https://condescending-mestorf-046396.netlify.app/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
